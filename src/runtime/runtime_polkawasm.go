9d83966758d601a61a6d0433256c432856e2e085
Rado M <radkomih@gmail.com>
2023-07-05 18:18:02 +0300

    polkawasm: add custom gc with external allocator

--- src/runtime/runtime_polkawasm.go
+++ src/runtime/runtime_polkawasm.go
@@ -2,6 +2,8 @@
 
 package runtime
 
+import "unsafe"
+
 // //export _start
 // func _start() {
 // 	// These need to be initialized early so that the heap can be initialized.
@@ -10,30 +12,36 @@ package runtime
 // 	run()
 // }
 
+// // Using global variables to avoid heap allocation.
+// const putcharBufferSize = 32 * 1024
+
+// var (
+// 	putcharBuffer        = [putcharBufferSize]byte{}
+// 	putcharPosition uint = 0
+// )
+
+// //go:export _debug_buf
+// func debugBuf() uintptr {
+// 	return uintptr(unsafe.Pointer(&putcharBuffer[0]))
+// }
+
 // Abort executes the wasm 'unreachable' instruction.
 func abort() {
 	trap()
 }
 
-//go:linkname os_runtime_args os.runtime_args
-func os_runtime_args() []string {
-	return []string{}
-}
-
-//go:linkname syscall_runtime_envs syscall.runtime_envs
-func syscall_runtime_envs() []string {
-	return []string{}
-}
-
 func putchar(c byte) {
+	// putcharBuffer[putcharPosition] = c
+	// putcharPosition++
+	panic("unimplemented: putchar")
 }
 
 func getchar() byte {
-	return 0
+	panic("unimplemented: getchar")
 }
 
 func buffered() int {
-	return 0
+	panic("unimplemented: buffered")
 }
 
 type timeUnit int64
@@ -55,19 +63,39 @@ func ticks() timeUnit {
 }
 
 //go:linkname now time.now
-func now() (int64, int32, int64) {
+func now() (sec int64, nsec int32, mono int64) {
 	panic("unimplemented: now")
 }
 
-//go:linkname syscall_Exit syscall.Exit
-func syscall_Exit(code int) {
-	return
+//go:linkname syscall_runtime_envs syscall.runtime_envs
+func syscall_runtime_envs() []string {
+	panic("unimplemented: syscall_runtime_envs")
 }
 
-//go:linkname procPin sync/atomic.runtime_procPin
-func procPin() {
+//go:linkname os_runtime_args os.runtime_args
+func os_runtime_args() []string {
+	return []string{}
 }
 
-//go:linkname procUnpin sync/atomic.runtime_procUnpin
-func procUnpin() {
-}
+// //go:linkname syscall_Exit syscall.Exit
+// func syscall_Exit(code int) {
+// 	return
+// }
+
+// //go:linkname procPin sync/atomic.runtime_procPin
+// func procPin() {
+//
+// }
+
+// //go:linkname procUnpin sync/atomic.runtime_procUnpin
+// func procUnpin() {
+//
+// }
+
+//go:wasm-module env
+//go:export ext_allocator_malloc_version_1
+func extalloc(size uintptr) unsafe.Pointer
+
+//go:wasm-module env
+//go:export ext_allocator_free_version_1
+func extfree(ptr unsafe.Pointer)
